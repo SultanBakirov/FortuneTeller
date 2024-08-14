3c2e8bef013799ee8b835c5316157d823efef024
Sultan Bakirov <sultanbek.bakirov.kk@gmail.com>
2024-08-14 06:10:25 +0200

    Fortune teller byself

---
+++ script.js
@@ -0,0 +1,8 @@
+let numberOfChildren = 2;
+let partnerName = "TaSamaya";
+let geographicLocation = "Paris";
+let jobTitle = "Software Engineer";
+
+let fortuneTeller = `You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnerName} with ${numberOfChildren} kids.`;
+
+document.getElementById("fortune").textContent = fortuneTeller;
